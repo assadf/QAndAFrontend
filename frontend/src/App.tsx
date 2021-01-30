6a86ad7e27d0deb732b1b6b20b14ca0cbefd8e89
Assad Faizi <13318157+assadf@users.noreply.github.com>
2021-01-30 17:58:08 +0000

    Chapter 4 - First Version of Routing Implemented

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,18 +5,35 @@ import { HomePage } from './HomePage';
 import { css, jsx } from '@emotion/core';
 import { fontFamily, fontSize, gray2 } from './Styles';
 
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import { AskPage } from './AskPage';
+import { SearchPage } from './SearchPage';
+import { SignInPage } from './SignInPage';
+import { NotFoundPage } from './NotFoundPage';
+import { QuestionPage } from './QuestionPage';
+
 function App() {
   return (
-    <div
-      css={css`
-        font-family: ${fontFamily};
-        font-size: ${fontSize};
-        color: ${gray2};
-      `}
-    >
-      <Header />
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <div
+        css={css`
+          font-family: ${fontFamily};
+          font-size: ${fontSize};
+          color: ${gray2};
+        `}
+      >
+        <Header />
+        <Switch>
+          <Redirect from="/home" to="/" />
+          <Route exact path="/" component={HomePage} />
+          <Route path="/search" component={SearchPage} />
+          <Route path="/ask" component={AskPage} />
+          <Route path="/signin" component={SignInPage} />
+          <Route path="/questions/:questionId" component={QuestionPage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/QuestionPage.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+import { Page } from './Page';
+import { RouteComponentProps } from 'react-router-dom';
+
+interface RouteParams {
+  questionId: string;
+}
+
+export const QuestionPage: FC<RouteComponentProps<RouteParams>> = ({
+  match,
+}) => <Page>Question Page {match.params.questionId} </Page>;
