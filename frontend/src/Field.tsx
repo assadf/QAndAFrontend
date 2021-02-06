7407e76e152a58121ebd3fe7944698b8cfdc8e13
Assad Faizi <13318157+assadf@users.noreply.github.com>
2021-02-06 10:09:20 +0000

    Added Validation to Forms

--- frontend/src/Field.tsx
+++ frontend/src/Field.tsx
@@ -30,7 +30,7 @@ const baseCSS = css`
 `;
 
 export const Field: FC<Props> = ({ name, label, type = 'Text' }) => {
-  const { setValue } = useContext(FormContext);
+  const { setValue, touched, setTouched, validate } = useContext(FormContext);
 
   const handleChange = (
     e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>,
@@ -38,11 +38,25 @@ export const Field: FC<Props> = ({ name, label, type = 'Text' }) => {
     if (setValue) {
       setValue(name, e.currentTarget.value);
     }
+    if (touched[name]) {
+      if (validate) {
+        validate(name);
+      }
+    }
+  };
+
+  const handleBlur = () => {
+    if (setTouched) {
+      setTouched(name);
+    }
+    if (validate) {
+      validate(name);
+    }
   };
 
   return (
     <FormContext.Consumer>
-      {({ values }) => (
+      {({ values, errors }) => (
         <div
           css={css`
             display: flex;
@@ -64,6 +78,7 @@ export const Field: FC<Props> = ({ name, label, type = 'Text' }) => {
                   id={name}
                   value={values[name] === undefined ? '' : values[name]}
                   onChange={handleChange}
+                  onBlur={handleBlur}
                   css={baseCSS}
                 />
               )}
@@ -76,10 +91,24 @@ export const Field: FC<Props> = ({ name, label, type = 'Text' }) => {
                     height: 100px;
                   `}
                   onChange={handleChange}
+                  onBlur={handleBlur}
                 />
               )}
             </label>
           )}
+          {errors[name] &&
+            errors[name].length > 0 &&
+            errors[name].map((error) => (
+              <div
+                key={error}
+                css={css`
+                  font-size: 12px;
+                  color: red;
+                `}
+              >
+                {error}
+              </div>
+            ))}
         </div>
       )}
     </FormContext.Consumer>
